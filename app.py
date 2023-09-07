a6b90d312cf273dae1ebd53340b50094d3db674e
they3ll0w <107738124+they3ll0w@users.noreply.github.com>
2023-09-07 18:37:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
 import turtle
-#Variable
+#Variables
 score_a=0
 score_b=0
 speed_ball = 30
 
 no = turtle.Turtle()
+
 #Functions
 
 #For paddle_a
-
 def paddle_a_up_down(speed):
     y = paddle_a.ycor() 
     y += speed
     paddle_a.sety(y)
-
-
+    
 #For paddle_b
-
 def paddle_b_up_down(speed):
     y = paddle_b.ycor() 
     y += speed
     paddle_b.sety(y)
 
 #Setting up the window
-
 wn = turtle.Screen()
 wn.title("Pong by Eric")
 wn.bgcolor("black")
@@ -40,7 +37,6 @@ paddle_a.penup()
 paddle_a.goto(-350,0)
 
 # Setting up Paddle B
-
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square") 
@@ -50,7 +46,6 @@ paddle_b.penup()
 paddle_b.goto(350,0)
 
 # Setting up the Ball
-
 ball = turtle.Turtle()
 ball.speed(0)  
 ball.shape("square")
@@ -61,7 +56,6 @@ ball.dx = 0.2
 ball.dy = 0.2
 
 #Setting up the scoreboard
-
 pen = turtle.Turtle()
 pen.speed(0)
 pen.color("white")
@@ -105,8 +99,8 @@ while True:
         score_b += 1
         pen.clear()
         pen.write(f"Player A:{score_a}  Player B:{score_b}", align="center", font=("Courier", 24, "normal"))
+        
     #Paddle and ball collision
-
     if (ball.xcor() > 340) and (ball.xcor() < 360) and (ball.ycor() < paddle_b.ycor() + 40) and (ball.ycor() > paddle_b.ycor() - 40) :
         ball.dx *= -1
     if (ball.xcor() < -340) and (ball.xcor() > -360) and (ball.ycor() < paddle_a.ycor() + 40) and (ball.ycor() > paddle_a.ycor() - 40) :
