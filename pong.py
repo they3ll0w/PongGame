793040844bc3fd8cfea4bd885dd8286675c9107b
they3ll0w <lieric2004@hotmail.com>
2023-12-22 23:58:26 -0500

    added classes to pong.py

--- pong.py
+++ pong.py
@@ -1,26 +1,28 @@
 import turtle
 
 #Variables
-score_a=0
-score_b=0
+score_a = 0
+score_b = 0
 speed_ball = 30
 
-no = turtle.Turtle()
+class Paddle:
+
+    def __init__(self, x, y):
+        self.turtle = turtle.Turtle()
+        self.turtle.speed(0)
+        self.turtle.shape("square")
+        self.turtle.shapesize(stretch_wid = 5, stretch_len = 1)
+        self.turtle.color("white")
+        self.turtle.penup() 
+        self.turtle.goto(x, y)
+
+    def paddle_movement(self, speed):
+        y = self.turtle.ycor() 
+        y += speed
+        self.turtle.sety(y)
 
 #Functions
-
-#For paddle_a
-def paddle_a_up_down(speed):
-    y = paddle_a.ycor() 
-    y += speed
-    paddle_a.sety(y)
-    
-#For paddle_b
-def paddle_b_up_down(speed):
-    y = paddle_b.ycor() 
-    y += speed
-    paddle_b.sety(y)
-
+        
 #Setting up the window
 wn = turtle.Screen()
 wn.title("Pong by Eric")
@@ -29,22 +31,10 @@ wn.setup(width=800, height=600)
 wn.tracer(0)
 
 # Setting up Paddle A
-paddle_a = turtle.Turtle()
-paddle_a.speed(0)  
-paddle_a.shape("square")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
-paddle_a.color("white")
-paddle_a.penup() 
-paddle_a.goto(-350,0)
+paddle_a = Paddle(-350, 0)
 
 # Setting up Paddle B
-paddle_b = turtle.Turtle()
-paddle_b.speed(0)
-paddle_b.shape("square") 
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
-paddle_b.color("white")
-paddle_b.penup()
-paddle_b.goto(350,0)
+paddle_b = Paddle(350, 0)
 
 # Setting up the Ball
 ball = turtle.Turtle()
@@ -62,16 +52,16 @@ pen.speed(0)
 pen.color("white")
 pen.penup()
 pen.hideturtle()
-pen.goto(0,260)
+pen.goto(0, 260)
 pen.write(f"Player A:{score_a}  Player B:{score_b}", align="center", font=("Courier", 24, "normal"))
 
 
 # Keyboard binding
 wn.listen() #listen to keyboard input
-wn.onkeypress(paddle_a_up_down(speed_ball), "w")
-wn.onkeypress(paddle_a_up_down(-speed_ball), "s")
-wn.onkeypress(paddle_b_up_down(speed_ball), "Up")
-wn.onkeypress(paddle_b_up_down(speed_ball), "Down")
+wn.onkey(lambda: paddle_a.paddle_movement(speed_ball), "w")
+wn.onkey(lambda: paddle_a.paddle_movement(-speed_ball), "s")
+wn.onkey(lambda: paddle_b.paddle_movement(speed_ball), "Up")
+wn.onkey(lambda: paddle_b.paddle_movement(-speed_ball), "Down")
 
 #Main game loop
 while True:
@@ -93,7 +83,6 @@ while True:
         ball.dx *= -1
         score_a += 1
         pen.clear()
-        pen.write(f"Player A:{score_a}  Player B:{score_b}", align="center", font=("Courier", 24, "normal"))
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
@@ -102,7 +91,7 @@ while True:
         pen.write(f"Player A:{score_a}  Player B:{score_b}", align="center", font=("Courier", 24, "normal"))
         
     #Paddle and ball collision
-    if (ball.xcor() > 340) and (ball.xcor() < 360) and (ball.ycor() < paddle_b.ycor() + 40) and (ball.ycor() > paddle_b.ycor() - 40) :
+    if (ball.xcor() > 340) and (ball.xcor() < 360) and (ball.ycor() < paddle_b.turtle.ycor() + 40) and (ball.ycor() > paddle_b.turtle.ycor() - 40) :
         ball.dx *= -1
-    if (ball.xcor() < -340) and (ball.xcor() > -360) and (ball.ycor() < paddle_a.ycor() + 40) and (ball.ycor() > paddle_a.ycor() - 40) :
+    if (ball.xcor() < -340) and (ball.xcor() > -360) and (ball.ycor() < paddle_a.turtle.ycor() + 40) and (ball.ycor() > paddle_a.turtle.ycor() - 40) :
         ball.dx *= -1
